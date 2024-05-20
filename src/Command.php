24463422cc161ee2035f53d6c8b19d1090e92012
Muhammad Awais <awais300@gmail.com>
2024-05-20 17:14:23 +0500

    test

--- src/Command.php
+++ src/Command.php
@@ -29,6 +29,7 @@ class Command {
 	 * Construct the class.
 	 */
 	public function __construct() {
+		exit('ddd');
 		add_action( 'cli_init', array( $this, 'wp_cli_register_commands' ) );
 		//$this->gdrive = GDrive::get_instance();
 	}
