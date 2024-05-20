ad1c481451937b6bfef8e66d8bcb3e2b24a836e5
Muhammad Awais <awais300@gmail.com>
2024-05-21 04:05:33 +0500

    PHP traits

---
+++ src/ReadWrite.php
@@ -0,0 +1,29 @@
+<?php
+namespace AwaisWP\GDriveWPCLIPackage;
+
+trait ReadWrite{
+	public function save_keys($data) {
+		if (file_put_contents(CREDENTIALS_PATH, $data) === false) {
+			    $error = error_get_last();
+			    \WP_CLI::error('Could not save keys. Reason: ' . $error);
+			}
+	}
+
+	/**
+     * Read keys from JSON file and convert to array
+     */
+    public function get_keys() {
+        $file_path = CREDENTIALS_PATH;
+
+        if ( ! file_exists( $file_path ) ) {
+            \WP_CLI::error( 'Keys file does not exist. Run "wp gd-upload addkey" to add them.' );
+        }
+
+        $keys_json = file_get_contents( $file_path );
+        $keys = json_decode( $keys_json, true );
+
+        return $keys;
+    }
+}
+
+?>
