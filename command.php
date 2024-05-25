970f794adb68157bf28777c26af4a27f6c7e813c
Muhammad Awais <awais300@gmail.com>
2024-05-26 03:54:00 +0500

    Update command.php

--- command.php
+++ command.php
@@ -9,12 +9,10 @@ if (file_exists($autoload)) {
     require_once $autoload;
 }
 
-use WP_CLI;
-
 // Define constatns.
 define('CREDENTIALS_PATH', __DIR__ . '/credentials.json');
 define('TOKEN_PATH', __DIR__ . '/token.json');
 define('SCOPES', implode(' ', array(\Google_Service_Drive::DRIVE_FILE)));
 
 // Register the main command with WP-CLI
-WP_CLI::add_command('gd-upload', '\AwaisWP\GDriveWPCLIPackage\Command');
+\WP_CLI::add_command('gd-upload', '\AwaisWP\GDriveWPCLIPackage\Command');
